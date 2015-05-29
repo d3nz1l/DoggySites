c84583abf04ffdc3e03e2e2a4651606d97d8b229
d3nz1l <mark.dendunnen@gmail.com>
2015-05-29 16:04:11 +0100

    Adding Sites

---
+++ Presentation/BristolBallistics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BristolBallistics.Startup))]
+namespace BristolBallistics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
