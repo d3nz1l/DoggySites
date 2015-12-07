a10cc0519edb2277e7de3b5e31056b305404533f
MarkDD <mark.dendunnen@gmail.com>
2015-12-07 08:29:31 +0000

    AngularTest

---
+++ AngularQuestionnaire/AngularQuestionnaire/Scripts/main.js
@@ -0,0 +1,28 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+var angular2_1 = require('angular2/angular2');
+var AppComponent = (function () {
+    function AppComponent() {
+        this.name = "John Doe";
+    }
+    AppComponent.prototype.hello = function () {
+        alert("hello, " + this.name);
+    };
+    AppComponent = __decorate([
+        angular2_1.Component({
+            selector: 'my-app'
+        }),
+        angular2_1.View({
+            directives: [angular2_1.formDirectives],
+            templateUrl: 'mainForm.html'
+        })
+    ], AppComponent);
+    return AppComponent;
+})();
+angular2_1.bootstrap(AppComponent);
